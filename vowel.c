0201d0a06a5772134eb6fa173a4ecd874e2eccda
Nandhinigunasekar <30343818+Nandhinigunasekar@users.noreply.github.com>
2017-08-11 16:59:51 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 { char n;
   clrscr();
   printf(" Enter the character : ");
   scanf("%c",&n);
   if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
-   printf(" The character is vowel ");
+   printf(" The character %c is vowel ");
   else
-   printf(" The character is consonant ");
+   printf(" The character %c is consonant ");
 }  
